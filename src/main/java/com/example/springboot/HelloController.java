4b921cda64c1dcf261400bf4eddb8d1897ac64a3
Haewon Shin <haewons@vmware.com>
2022-07-15 08:03:28 +0900

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String str = "Greetings from Spring Boot + Tanzu!";
+		String str = "Greetings from Spring Boot + Tanzu! Welcome!!!";
 		return str;
 	}
 
